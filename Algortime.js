241f0b43d7a3cc7b17e93696307a690aa862a468
Thibaut <thibaut.hellinckx@student.ehb.be>
2021-08-21 17:43:06 +0200

    Added the list of games in algoritme

---
+++ Algortime.js
@@ -0,0 +1,22 @@
+var games = [{
+    name: "Donkey Kong",
+    rating: "4"
+}, {
+    name: "Tetris",
+    rating: "5"
+}, {
+    name: "Pac man",
+    rating: "1"
+}, {
+    name: "Space Invaders",
+    rating: "3"
+}, {
+    name: "Street Fighter",
+    rating: "2"
+}, {
+    name: "After Burner",
+    rating: "1"
+}];
+
+var gameNames = ["After Burner", "Street Fighter", "Space Invaders", "Pac man", "Tetris", "Donkey Kong"];
+
