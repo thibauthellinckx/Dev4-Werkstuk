a595ebb91b5231bb88116b08d66f868c63fa4f27
Thibaut <thibaut.hellinckx@student.ehb.be>
2021-08-22 18:22:32 +0200

    refactor: added list of games and also added first iterator function

---
+++ Iterator.js
@@ -0,0 +1,61 @@
+const games = [{
+    name: "Donkey Kong",
+    rating: 4,
+    genre: "adventure"
+}, {
+    name: "Zombie Raid",
+    rating: 5,
+    genre: "horror"
+}, {
+    name: "After Burner",
+    rating: 1,
+    genre: "horror"
+}, {
+    name: "Pac man",
+    rating: 1,
+    genre: "adventure"
+}, {
+    name: "Space Invaders",
+    rating: 3,
+    genre: "adventure"
+}, {
+    name: "Street Fighter",
+    rating: 2,
+    genre: "horror"
+}, {
+    name: "Super mario ball",
+    rating: 5,
+    genre: "adventure"
+}, {
+    name: "Tetris",
+    rating: 5,
+    genre: "adventure"
+}];
+
+function Iterator(start, end, step, game) {
+    let nextIndex = start;
+    let iterationCount = 0;
+
+    const gameIterator = {
+        next: function () {
+            let result;
+
+            if (nextIndex < end) {
+                result = {
+                    name: game[nextIndex].name,
+                    rating: game[nextIndex].rating,
+                    genre: game[nextIndex].genre,
+                    done: false
+                };
+                nextIndex += step;
+                iterationCount++;
+                return result;
+            }
+            return {
+                value: iterationCount,
+                done: true
+            };
+        }
+    };
+    return gameIterator;
+}
