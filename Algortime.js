4f0c8563a34c9dc0c54d88ab51ea531829c0d57a
Thibaut <thibaut.hellinckx@student.ehb.be>
2021-08-23 22:24:20 +0200

    fix: fixed the game list, made it more detailed and longer

--- Algortime.js
+++ Algortime.js
@@ -1,22 +1,42 @@
-var games = [{
+const games = [{
     name: "Donkey Kong",
-    rating: "4"
+    rating: 4,
+    genre: "adventure"
 }, {
     name: "Tetris",
-    rating: "5"
+    rating: 5,
+    genre: "arcade/puzzel"
 }, {
     name: "Pac man",
-    rating: "1"
+    rating: 1,
+    genre: "adventure"
 }, {
     name: "Space Invaders",
-    rating: "3"
+    rating: 3,
+    genre: "adventure"
 }, {
     name: "Street Fighter",
-    rating: "2"
+    rating: 2,
+    genre: "fighting"
 }, {
     name: "After Burner",
-    rating: "1"
+    rating: 1,
+    genre: "action"
+},{
+    name: "Lover",
+    rating: 5,
+    genre: "love"
+}, {
+    name: "Xevious",
+    rating: 3,
+    genre: "action"
+
+}, {
+    name: "Robotron 2084",
+    rating: 5,
+    genre: "adventure"
+
 }];
 
-var gameNames = ["After Burner", "Street Fighter", "Space Invaders", "Pac man", "Tetris", "Donkey Kong"];
+
 
